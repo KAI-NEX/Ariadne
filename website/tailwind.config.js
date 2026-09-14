8b01ac3b30c1a3ba3c43b772a5936b72ad8439bb
KAI <kleinguo9@gmail.com>
2026-09-14 22:06:09 +0800

    chore: remove standalone website and animation experiments

--- website/tailwind.config.js
+++
@@ -1,6 +0,0 @@
-export default {
-  content: ['./index.html', './src/**/*.{js,jsx}'],
-  corePlugins: { preflight: false },
-  theme: { extend: { fontFamily: { geist: ['var(--vi-font-site)'] } } },
-  plugins: [],
-};
